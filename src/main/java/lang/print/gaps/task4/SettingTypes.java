fff491059e356980ba0c465f4af853ecc8f8cf6e
Artak1999 <dashyan.1999@mail.ru>
2022-07-04 13:57:54 +0500

    Not hard.

--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -9,6 +9,13 @@ public class SettingTypes {
 //        fifth = 12.3F;
 //        sixth = 1;
 //        seventh = 129;
+//        first = 10_000_000_000L;
+//        second = 'a';
+//        third = 5 > 9;
+//        forth = 12.90434323324342;
+//        fifth = 12.3F;
+//        sixth = 1;
+//        seventh = 129;
          long first = 10_000_000_000L;
          char second = 'a';
          boolean third = 5 > 9;
