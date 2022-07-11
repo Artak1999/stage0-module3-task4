25e12f1a4042de3ac04f00b08618772d820871f1
Artak1999 <dashyan.1999@mail.ru>
2022-07-11 16:48:12 +0500

    Not hard

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,7 +4,6 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a = 2;
         int b = 2;
-        int c = ((9*(a*a)) - (5*b) + 2 + a - 7) * ((a + b - (4*a*b))/2);
-        System.out.println(c);
+        System.out.println((9*(a*a) - (5*b) + 2 + a - 7) * (a + b - (4*a*b))/2);
     }
 }
